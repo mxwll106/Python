3f169d2f508bdcd066470ee0963e6d7020a67998
Courtnee Wallace <26284945-CourtneeWallace@users.noreply.replit.com>
2024-09-06 18:06:17 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+print ("Positive Affirmation Machine")
+print ()
+name = input("What is your name:")
+if name == "Joi" or name == "joi":
+  print("Hi Joi!")
+print ()
+achievement = input("What do you want to achieve?")
+if achievement == "Excellent Programmer" or achievement == "excellent programmer":
+  print("That's Wonderful!")
+  print()
+  print ("On a scale of 1-10 how do you feel today?")
+  feeling = input("1-10:")
+  if feeling == "1" or feeling == "2" or feeling == "3":
+    print()
+    print("Hello", name, "keep your head on a swivle! Today you're going to become a", 
+          achievement,
+          "you will be the best in the field, continue working hard -YOU'RE AMAZING!")
+    #replit100DaysOfCode
